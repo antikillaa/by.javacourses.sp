51c67a6b06de2a8741beb306d89d2db7e1604cab
stanislav.peshkur <antikillaaaa@gmail.com>
2018-02-13 18:10:41 +0300

    math

---
+++ src/main/java/by/javacourse/sp/math/bymethods/Math.java
@@ -0,0 +1,16 @@
+package by.javacourse.sp.math.bymethods;
+
+public class Math {
+    public static void main(String[] args) {
+    Steps doings = new Steps();
+    doings.setX( 5 );
+        int x = doings.getX();
+    doings.setY( 6 );
+        int y = doings.getY();
+    doings.setZ( 0 );
+        int z = doings.getZ();
+
+    Steps.One();
+
+    }
+}
